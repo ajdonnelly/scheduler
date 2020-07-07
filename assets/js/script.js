be7298ed5d35989671fd82df6242df213dfa142c
alex donnelly <a.james.donnelly@gmail.com>
2020-07-06 22:36:52 -0500

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,7 @@ task2pm = JSON.parse(localStorage.getItem("task-2pm"));
 $(".2pm").html(task2pm);
 
 task3pm = JSON.parse(localStorage.getItem("task-3pm"));
-$(".3pm").html(task3m);
+$(".3pm").html(task3pm);
 
 task4pm = JSON.parse(localStorage.getItem("task-4pm"));
 $(".4pm").html(task4pm);
@@ -36,6 +36,7 @@ $(".5pm").html(task5pm);
 
 
 //save all tasks
+//9am
 $(".saveBtn-9am").click(function() {
 
 var taskText = $("#modalTaskDescription-9am").val();
@@ -45,6 +46,7 @@ loadTasks9am();
 
 });
 
+//10am
 $(".saveBtn-10am").click(function() {
 
 var taskText = $("#modalTaskDescription-10am").val();
@@ -54,16 +56,124 @@ loadTasks10am();
 
 });
 
+//11am
+$(".saveBtn-11am").click(function() {
+
+  var taskText = $("#modalTaskDescription-11am").val();
+  localStorage.setItem("task-11am", JSON.stringify(taskText));
+  
+  loadTasks11am();
+  
+  });
+
+//12pm
+$(".saveBtn-12pm").click(function() {
+
+  var taskText = $("#modalTaskDescription-12pm").val();
+  localStorage.setItem("task-12pm", JSON.stringify(taskText));
+  
+  loadTasks12pm();
+  
+  });
+//1pm
+$(".saveBtn-1pm").click(function() {
+
+  var taskText = $("#modalTaskDescription-1pm").val();
+  localStorage.setItem("task-1pm", JSON.stringify(taskText));
+  
+  loadTasks1pm();
+  
+  });
+
+//2pm
+$(".saveBtn-2pm").click(function() {
+
+  var taskText = $("#modalTaskDescription-2pm").val();
+  localStorage.setItem("task-2pm", JSON.stringify(taskText));
+  
+  loadTasks2pm();
+  
+  });
+
+//3pm
+$(".saveBtn-3pm").click(function() {
+
+  var taskText = $("#modalTaskDescription-3pm").val();
+  localStorage.setItem("task-3pm", JSON.stringify(taskText));
+  
+  loadTasks3pm();
+  
+  });
+
+//4pm
+$(".saveBtn-4pm").click(function() {
+
+  var taskText = $("#modalTaskDescription-4pm").val();
+  localStorage.setItem("task-4pm", JSON.stringify(taskText));
+  
+  loadTasks4pm();
+  
+  });
+//5pm
+$(".saveBtn-5pm").click(function() {
+
+  var taskText = $("#modalTaskDescription-5pm").val();
+  localStorage.setItem("task-5pm", JSON.stringify(taskText));
+  
+  loadTasks5pm();
+  
+  });
+
+
 //load new tasks
+
+//9am
 var loadTasks9am = function() {
 task9am = JSON.parse(localStorage.getItem("task-9am"));
 $(".9am").html(task9am);
 };
 
+//10am
 var loadTasks10am = function() {
-task9am = JSON.parse(localStorage.getItem("task-10am"));
+task10am = JSON.parse(localStorage.getItem("task-10am"));
 $(".10am").html(task10am);
 };
+
+//11am
+var loadTasks11am = function() {
+  task11am = JSON.parse(localStorage.getItem("task-11am"));
+  $(".11am").html(task11am);
+  };
+//12pm
+var loadTasks12pm = function() {
+  task12pm = JSON.parse(localStorage.getItem("task-12pm"));
+  $(".12pm").html(task12pm);
+  };
+//1pm
+var loadTasks1pm = function() {
+  task1pm = JSON.parse(localStorage.getItem("task-1pm"));
+  $(".1pm").html(task1pm);
+  };
+//2pm
+var loadTasks2pm = function() {
+  task2pm = JSON.parse(localStorage.getItem("task-2pm"));
+  $(".2pm").html(task2pm);
+  };
+//3pm
+var loadTasks3pm = function() {
+  task3pm = JSON.parse(localStorage.getItem("task-3pm"));
+  $(".3pm").html(task3pm);
+  };
+//4pm
+var loadTasks4pm = function() {
+  task4pm = JSON.parse(localStorage.getItem("task-4pm"));
+  $(".4pm").html(task4pm);
+  };
+//5pm
+var loadTasks5pm = function() {
+  task5pm = JSON.parse(localStorage.getItem("task-5pm"));
+  $(".5pm").html(task5pm);
+  };
 
 //set color coding of timeblocks
 
